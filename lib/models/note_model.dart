6908a31c8cc20b7828d98138e20eb0aee8cfd324
AQ <cshazemelwazery@gmail.com>
2024-02-19 19:46:46 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String description;
+  final String date;
+  final int color;
+  NoteModel(
+      {required this.title,
+      required this.description,
+      required this.color,
+      required this.date});
+}
