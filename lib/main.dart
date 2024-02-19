3744490cb9ac9e500cf1f852ea7aae22ec30024c
AQ <cshazemelwazery@gmail.com>
2024-02-19 23:05:05 +0200

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+import 'constants.dart';
+
+void main() async{
+  await Hive.initFlutter();
+ await Hive.openBox(kNotesName);
   runApp(const NotesApp());
 }
 
