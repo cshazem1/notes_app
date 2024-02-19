841b2f8f0a613ac343956fbb343cb0cf27ff94e3
AQ <cshazemelwazery@gmail.com>
2024-02-20 00:34:37 +0200

    take input and validate data

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/widgets/custom_button.dart';
@@ -8,34 +10,78 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomTextField(
-              hintText: "Enter Your Title",
-              labelText: "Title",
-              onSubmitted: (p0) {},
-              onChange: (p0) {},
-            ),
-            CustomTextField(
-              hintText: "Enter Your Description",
-              labelText: "Description",
-              maxLine: 5,
-              onSubmitted: (p0) {},
-              onChange: (p0) {},
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              text: "Add",
-              onClick: () {},
-              color: kPrimaryColor,
-            )
-          ],
-        ),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10),
+      child: SingleChildScrollView(child: AddNoteForm()),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  AutovalidateMode? autoValidateMode =AutovalidateMode.disabled;
+  String ?title;
+  String? description;
+ final GlobalKey<FormState> globalKey=GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      autovalidateMode:autoValidateMode ,
+      key: globalKey,
+      child: Column(
+        children: [
+          CustomTextField(
+            hintText: "Enter Your Title",
+            labelText: "Title",
+            onSubmitted: (data) {
+              title=data;
+              setState(() {
+log(title!);
+              });
+            },
+            onChange: (p0) {},
+          ),
+          CustomTextField(
+            hintText: "Enter Your Description",
+            labelText: "Description",
+            maxLine: 5,
+            onSubmitted: (data) {
+              description=data;
+              setState(() {
+                log(description!);
+
+              });
+            },
+            onChange: (p0) {},
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButton(
+            text: "Add",
+            onClick: () {
+              if(globalKey.currentState!.validate())
+              {
+                globalKey.currentState!.save();
+              }
+              else {
+                autoValidateMode = AutovalidateMode.always;
+                setState(() {
+
+                });
+              }
+            },
+            color: kPrimaryColor,
+          )
+        ],
       ),
     );
   }
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -6,7 +6,7 @@ import 'package:notes_app/constants.dart';
 // ignore: must_be_immutable
 class CustomTextField extends StatelessWidget {
   final Function(String) onChange;
-  final Function(String) onSubmitted;
+  final void Function(String?)? onSubmitted;
   final String? initialText;
   final String? hintText;
   final String? labelText;
@@ -31,10 +31,19 @@ class CustomTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextField(
+      child: TextFormField(
+        validator: (value) {
+          if(value?.isEmpty??true) {
+            return "field is required";
+          }
+          else {
+            return null;
+          }
+        },
+
         maxLines:maxLine ,
         enabled: enable,
-        onSubmitted: onSubmitted,
+        onSaved: onSubmitted,
         onChanged: onChange,
         decoration: InputDecoration(
             suffixIcon: icon,
